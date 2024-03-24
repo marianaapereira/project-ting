36056bd6d22dd25d2ebb1f6f57f4ca6a97f096ad
Mariana Pereira <marianapereira.s@live.com>
2024-03-24 01:30:08 -0300

    Correções de lint

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,7 +1,12 @@
 from ting_file_management import file_management
 
+
 def process(path_file, instance):
-    file_exists = any(existent_file["nome_do_arquivo"] == path_file for existent_file in instance.queue)
+    file_exists = any(
+        (
+            existent_file["nome_do_arquivo"] == path_file
+        ) for existent_file in instance.queue
+    )
 
     if not file_exists:
         file_lines = file_management.txt_importer(path_file)
