6846b2137757a211d446c14d01c42c05025d6e34
Mariana Pereira <marianapereira.s@live.com>
2024-03-24 03:04:31 -0300

    Função search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -23,4 +23,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data_list = list()
+
+    for existent_file in instance.queue:
+        index_list = list()
+        file_lines = existent_file["linhas_do_arquivo"]
+
+        for line in file_lines:
+            if word.lower() in line.lower():
+                index = file_lines.index(line) + 1
+                index_list.append({"linha": index, "conteudo": line})
+
+        if len(index_list) > 0:
+            data = {
+                "palavra": word,
+                "arquivo": existent_file["nome_do_arquivo"],
+                "ocorrencias": index_list
+            }
+
+            data_list.append(data)
+
+    return data_list
